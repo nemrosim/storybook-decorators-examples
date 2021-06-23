f0bdf4adf1f1c51d1cf4d45ea743c4766fb4e2f1
nemrosim <nemrosim1988@gmail.com>
2021-06-23 20:27:12 +0300

    clean up project, fix babel issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { green, purple } from "@material-ui/core/colors";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: purple[500],
+        },
+        secondary: {
+            main: green[500],
+        },
+    },
+});
 
-export default App;
+export const App = () => (
+    <ThemeProvider theme={theme}>
+        <div className="App">
+            Hello world
+        </div>
+    </ThemeProvider>
+);
