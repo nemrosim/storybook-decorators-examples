80cd0ddbcc06f110a32c96012820bb46ba1c1378
nemrosim <nemrosim1988@gmail.com>
2021-06-24 17:51:15 +0300

    remove not used Storybook args

--- src/stories/Button/Button.stories.tsx
+++ src/stories/Button/Button.stories.tsx
@@ -5,44 +5,17 @@ import { Button, ButtonProps } from "@material-ui/core";
 export default {
     component: Button,
     title: 'MaterialUI/Button',
-    argTypes: {
-        children: {
-            type: 'string',
-            name: 'buttonText',
-            defaultValue: 'Button text',
-        },
-        disabled: {
-            type: 'boolean',
-            defaultValue: false,
-        },
-        fullWidth: {
-            type: 'boolean',
-            defaultValue: true
-        },
-        variant: {
-            name: 'Some name',
-            description: 'Some description',
-            defaultValue: 'contained',
-            options: ['contained', 'outlined', 'text'],
-            control: {
-                type: 'inline-radio'
-            }
-        },
-        color: {
-            defaultValue: 'primary',
-            options: ['default', 'inherit', 'primary', 'secondary'],
-            control: {
-                type: 'select'
-            }
-        }
-    }
 } as Meta;
 
 const Template: Story<ButtonProps> = (args) => (
-    <Button {...args} >{args.children}</Button>
+    <Button
+        {...args}
+        fullWidth={true}
+        color='primary'
+        variant='contained'
+    >
+        Button text
+    </Button>
 );
 
 export const Primary = Template.bind({});
-Primary.args = {
-    variant: "contained",
-};
